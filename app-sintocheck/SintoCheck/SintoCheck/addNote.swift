346bed54306715d9be512228ef17e44ef09810b7
sebastian <sebramirezcordero@gmail.com>
2023-11-13 01:36:08 -0600

    API connection notes

--- app-sintocheck/SintoCheck/SintoCheck/addNote.swift
+++ app-sintocheck/SintoCheck/SintoCheck/addNote.swift
@@ -7,9 +7,60 @@
 
 import SwiftUI
 
+
+//si quiero obtener cuando se creo la nota aqui se puede agregar.
+struct Note: Codable, Identifiable {
+    let id: String
+    let title: String
+    let content: String
+}
+
+func addNoteAPI(title: String, content: String, completion: @escaping (Note?) -> Void) {
+    //obtener id del usuario que inicio sesion esta hardcodeado ahora
+    guard let url = URL(string: "https://sintocheck-backend.vercel.app/note") else {return}
+    var request = URLRequest(url: url)
+    //obtener token del inicio de sesion esta hardcodeado ahora.
+    request.addValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NGVhMDlmZDlmYjc5MWI0YjdmMDg3YyIsIm5hbWUiOiJQYWNpZW50ZSBDZXJvIiwicGhvbmUiOiIwOTg3NjU0MzIxIiwiaWF0IjoxNjk5NjUxODE4LCJleHAiOjE3MDA4NjE0MTh9.Z_WvGy2TCsvFr9_eW_V3ModNnupaUr1_B9QtNG7I97A", forHTTPHeaderField: "Authorization")
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    let bodyData = [
+        //tomar doctorId como parametro a la funcion
+        "title" : title,
+        "content" : content,
+        //obtener del login no hardcodeado como ahora.
+        "patientId" : "654ea09fd9fb791b4b7f087c"
+    ]
+    do {
+        request.httpBody = try JSONSerialization.data(withJSONObject: bodyData, options: [])
+    } catch {
+        print("Error encoding data: \(error)")
+    }
+
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        if let error = error {
+            print("Error: \(error)")
+        }
+        if let response = response {
+            print("Response: \(response)")
+        }
+        if let data = data {
+            do {
+                let decoder = JSONDecoder()
+                let doctoresAPI = try decoder.decode(Note.self, from: data)
+                print("Data: \(doctoresAPI)")
+                completion(doctoresAPI)
+            } catch {
+                print("Error decoding data: \(error)")
+                completion(nil)
+            }
+        }
+    }.resume()
+}
+
 struct addNote: View {
     @State var titulo = ""
     @State var contenido = ""
+    @State var note: Note?
     var body: some View {
         VStack {
             Spacer()
@@ -30,7 +81,9 @@ struct addNote: View {
                 .padding(.horizontal, 20)
             Spacer()
             Button {
-                
+                addNoteAPI(title: titulo, content: contenido) { note in
+                    self.note = note
+                }
             } label: {
                 ZStack {
                     RoundedRectangle(cornerRadius: 10)
