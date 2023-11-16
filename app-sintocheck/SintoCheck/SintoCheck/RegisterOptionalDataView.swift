4ec994b3c2f3a0b03da17c55f6a0ec9d7b4db13a
alexajro <A00833943@TEC.MX>
2023-11-15 22:57:15 -0600

    Added validation to the register views

--- app-sintocheck/SintoCheck/SintoCheck/RegisterOptionalDataView.swift
+++ app-sintocheck/SintoCheck/SintoCheck/RegisterOptionalDataView.swift
@@ -9,37 +9,54 @@ import SwiftUI
 
 struct RegisterOptionalDataView: View {
     @State var birthDate = Date()
-    @State var phone = ""
-    @State var pass = ""
-    @State var ConfirmPass = ""
+    @State var height = ""
+    @State var weight = ""
+    @State var medicine = ""
+    @State var background = ""
+    @State var mostrarRegistro = false
+    
     let rojo = Color(red: 148/255, green: 28/255, blue: 47/255)
     let azul = Color(red: 26/255, green: 26/255, blue: 102/255)
     var body: some View {
             VStack (spacing: 0){
-            Image("Logo")
-                .frame(width: 100, height: 150)
-            Text("Registro")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(Color.black)
-            Text("Introduce tu Informacion")
-                .font(.title3)
-            Text("Opcional")
-                .font(.caption)
-                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-            DatePicker("Fecha de Nacimiento", selection: $birthDate, displayedComponents: .date)
-                .datePickerStyle(CompactDatePickerStyle())
-                .padding(.top, 25)
+                HStack{
+                    Button(action: {
+                        mostrarRegistro = true
+                    }){
+                        Image(systemName: "arrow.left")
+                        Text("Regresar")
+                            .foregroundColor(azul)
+                    }
+                    .fullScreenCover(isPresented: $mostrarRegistro){
+                        RegisterView()
+                    }
+                    .foregroundColor(azul)
+                    Spacer()
+                }
+                Image("Logo")
+                    .frame(width: 100, height: 150)
+                Text("Registro")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.black)
+                Text("Introduce tu Informacion")
+                    .font(.title3)
+                Text("Opcional")
+                    .font(.caption)
+                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                DatePicker("Fecha de Nacimiento", selection: $birthDate, displayedComponents: .date)
+                    .datePickerStyle(CompactDatePickerStyle())
+                    .padding(.top, 25)
                 HStack{
                     VStack{
                         Text("Estatura")
                             .padding(.top, 25)
                             .padding(.bottom, 15)
                         HStack{
-                            TextField("Estatura", text: $phone)
+                            TextField("Estatura", text: $height)
                                 .frame(height: 5)
                                 .padding()
-                                .background(RoundedRectangle(cornerRadius: 4).stroke(phone != "" ? Color(Color(red: 148/255, green: 28/255, blue: 47/255)) : Color.black, lineWidth: 2))
+                                .background(RoundedRectangle(cornerRadius: 4).stroke(height != "" ? Color(Color(red: 148/255, green: 28/255, blue: 47/255)) : Color.black, lineWidth: 2))
                                 .keyboardType(.numberPad)
                             Text("m")
                         }
@@ -49,43 +66,43 @@ struct RegisterOptionalDataView: View {
                             .padding(.top, 25)
                             .padding(.bottom, 15)
                         HStack{
-                            TextField("Peso", text: $pass)
+                            TextField("Peso", text: $weight)
                                 .frame(height: 5)
                                 .padding()
-                                .background(RoundedRectangle(cornerRadius: 4).stroke(pass != "" ? Color(Color(red: 148/255, green: 28/255, blue: 47/255)) : Color.black, lineWidth: 2))
+                                .background(RoundedRectangle(cornerRadius: 4).stroke(weight != "" ? Color(Color(red: 148/255, green: 28/255, blue: 47/255)) : Color.black, lineWidth: 2))
                                 .keyboardType(.numberPad)
-                        Text("kg")
+                            Text("kg")
                         }
                     }
                 }
-            Text("Medicamentos")
+                Text("Medicamentos")
+                    .padding(.top, 25)
+                    .padding(.bottom, 15)
+                TextField("Medicamentos", text: $medicine)
+                    .frame(height: 5)
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 4).stroke(medicine != "" ? Color(Color(red: 148/255, green: 28/255, blue: 47/255)) : Color.black, lineWidth: 2))
+                Text("Antecedentes")
+                    .padding(.top, 25)
+                    .padding(.bottom, 15)
+                TextField("Antecedentes", text: $background)
+                    .frame(height: 25)
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 4).stroke(background != "" ? Color(Color(red: 148/255, green: 28/255, blue: 47/255)) : Color.black, lineWidth: 2))
+                    .padding(.bottom, 25)
+                Button(action: {
+                }){
+                    Text("Registrar")
+                        .foregroundColor(.white)
+                        .padding(.vertical)
+                        .frame(width: UIScreen.main.bounds.width - 50)
+                }
+                .background(azul)
+                .cornerRadius(10)
                 .padding(.top, 25)
-                .padding(.bottom, 15)
-            TextField("Medicamentos", text: $pass)
-                .frame(height: 5)
-                .padding()
-                .background(RoundedRectangle(cornerRadius: 4).stroke(pass != "" ? Color(Color(red: 148/255, green: 28/255, blue: 47/255)) : Color.black, lineWidth: 2))
-            Text("Antecedentes")
-                .padding(.top, 25)
-                .padding(.bottom, 15)
-            TextField("Antecedentes", text: $pass)
-                .frame(height: 25)
-                .padding()
-                .background(RoundedRectangle(cornerRadius: 4).stroke(pass != "" ? Color(Color(red: 148/255, green: 28/255, blue: 47/255)) : Color.black, lineWidth: 2))
-                .padding(.bottom, 25)
-            Button(action: {
-            }){
-                Text("Registrar")
-                    .foregroundColor(.white)
-                    .padding(.vertical)
-                    .frame(width: UIScreen.main.bounds.width - 50)
             }
-            .background(azul)
-            .cornerRadius(10)
-            .padding(.top, 25)
+            .padding(.horizontal, 25)
         }
-        .padding(.horizontal, 25)
-    }
     }
 
 struct RegisterOptionalData_Previews: PreviewProvider {
--- app-sintocheck/SintoCheck/SintoCheck/RegisterView.swift
+++ app-sintocheck/SintoCheck/SintoCheck/RegisterView.swift
@@ -14,6 +14,8 @@ struct RegisterView: View {
     @State var ConfirmPass = ""
     @State var mostrarLogin = false
     @State var mostrarDatosAdicionales = false
+    @State var missingData = false
+    @State var unmatchedPasswords = false
     let rojo = Color(red: 148/255, green: 28/255, blue: 47/255)
     let azul = Color(red: 26/255, green: 26/255, blue: 102/255)
     var body: some View {
@@ -53,19 +55,32 @@ struct RegisterView: View {
             Text("Confirma tu Contraseña")
                 .padding(.top, 25)
                 .padding(.bottom, 15)
-            SecureField("Contraseña", text: $pass)
+            SecureField("Contraseña", text: $ConfirmPass)
                 .frame(height: 5)
                 .padding()
                 .background(RoundedRectangle(cornerRadius: 4).stroke(pass != "" ? Color(Color(red: 148/255, green: 28/255, blue: 47/255)) : Color.black, lineWidth: 2))
                 .padding(.bottom, 25)
             Button(action: {
-                mostrarDatosAdicionales = true
+                
+                if nombre != "" &&
+                   phone != "" &&
+                    pass != "" &&
+                    ConfirmPass != "" && pass == ConfirmPass{
+                    mostrarDatosAdicionales = true
+                } else if pass != ConfirmPass {
+                    unmatchedPasswords = true
+                } else{
+                    missingData = true
+                }
+                
             }){
                 Text("Siguiente")
                     .foregroundColor(.white)
                     .padding(.vertical)
                     .frame(width: UIScreen.main.bounds.width - 50)
             }
+            .alert("Por favor llena todos los datos", isPresented : $missingData, actions: {})
+            .alert("Las contraseñas no coinciden", isPresented : $unmatchedPasswords, actions: {})
             .fullScreenCover(isPresented: $mostrarDatosAdicionales){
                     RegisterOptionalDataView()
                 }
@@ -90,6 +105,7 @@ struct RegisterView: View {
         .padding(.horizontal, 25)
     }
 }
+
 
 struct RegisterView_Previews: PreviewProvider {
     static var previews: some View {
