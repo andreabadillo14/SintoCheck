9509d0cbb3595744c7244381063507d53d5d588d
Andrea Badillo <A00833511@tec.mx>
2023-11-18 22:18:31 -0600

    Update AuthenticationResponse.swift

--- app-sintocheck/SintoCheck/SintoCheck/Types/Responses/AuthenticationResponse.swift
+++ app-sintocheck/SintoCheck/SintoCheck/Types/Responses/AuthenticationResponse.swift
@@ -8,8 +8,16 @@
 import Foundation
 
 class AuthenticationResponse: Codable {
-    var token: String
-    var id: Int
+    var id: String
     var name: String
     var phone: String
+    var password : String
+    var birthdate : String?
+    var height : Float?
+    var weight : Float?
+    var medicine : String?
+    var medicalBackground : String?
+    var doctorIds : [String]?
+    var createdAt : String
+    var token: String
 }
