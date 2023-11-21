2f2856f7c82a2c987d545db3cd08dac3301e3c17
Andrea Badillo <A00833511@tec.mx>
2023-11-20 23:45:42 -0600

    Ya jala

--- app-sintocheck/SintoCheck/SintoCheck/ProfileView.swift
+++ app-sintocheck/SintoCheck/SintoCheck/ProfileView.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 struct ProfileView: View {
     
+    @State var patientData : AuthenticationResponse?
+    //@State var APatient : PatientSignupRequest
+    
+//    var patients = [
+//        Patient(id: UUID(), name: "Hermenegildo", lastname: "Pérez", birthdate: "1945-03-25", height: 1.78, weight: 65.4, medicine: "Vitaminas de calcio", medicalBackground: "Genética de diabetes")
+//    ]
+    
     enum FileReaderError: Error {
         case fileNotFound
         case fileReadError
@@ -18,7 +25,7 @@ struct ProfileView: View {
         // Retrieve the file URL
         let sandboxURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
         let fileURL = sandboxURL.appendingPathComponent("loginResponse.json")
-
+        
         // Check if the file exists
         let fileManager = FileManager.default
         
@@ -26,7 +33,7 @@ struct ProfileView: View {
             // File does not exist, handle this case accordingly
             fatalError("Could not find login response file")
         }
-
+        
         // Read the file contents
         if let data = fileManager.contents(atPath: fileURL.path) {
             // Decode the JSON data into a PatientSignupResponse object
@@ -38,141 +45,161 @@ struct ProfileView: View {
             
             throw FileReaderError.fileNotFound        }
     }
-
+    
     func handlePatientData() {
-           do {
+        do {
+            let sandboxURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            let fileURL = sandboxURL.appendingPathComponent("loginResponse.json")
+
+            if !FileManager.default.fileExists(atPath: fileURL.path) {
+                // File does not exist, handle this case accordingly
+                print("Login response file not found")
+                return
+            }
+
             patientData = try getPatientData()
-           } catch let error as FileReaderError {
-               switch error {
-               case .fileNotFound:
-                   print("File not found")
-               case .fileReadError:
-                   print("Error reading file")
-               }
-           } catch {
-               print("An unknown error occurred: \(error)")
-           }
-       }
-}   
+        } catch let error {
+            print("An error occurred while retrieving patient data: \(error)")
+        }
+    }
+
+    
+//    func handlePatientData() {
+//        do {
+//            patientData = try getPatientData()
+//        } catch let error as FileReaderError {
+//            switch error {
+//            case .fileNotFound:
+//                print("File not found")
+//            case .fileReadError:
+//                print("Error reading file")
+//            }
+//        } catch {
+//            print("An unknown error occurred: \(error)")
+//        }
+//    }
+    
     var body: some View {
         NavigationView {
-            ZStack {
-                Color("Backgrounds")
-                    .ignoresSafeArea()
-                VStack {
+            VStack {
+                ZStack {
+                    Color("Backgrounds")
+                        .ignoresSafeArea()
                     VStack {
-                        Text("Mi perfil")
-                            .bold()
-                            .font(.largeTitle)
-                            .frame(maxWidth: .infinity, alignment: .leading)
+                        VStack {
+                            Text("Mi perfil")
+                                .bold()
+                                .font(.largeTitle)
+                                .frame(maxWidth: .infinity, alignment: .leading)
                             
                             
-                        HStack(alignment: .center) {
-                            Image(systemName: "person.circle.fill")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 73)
-                                .padding(.leading, -60)
-                            
-                            VStack(alignment: .leading) { // esto sigue hardcordeado
-                                Text(patients[0].name)
-                                Text(patients[0].phone)
+                            HStack(alignment: .center) {
+                                Image(systemName: "person.circle.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 73)
+                                    .padding(.leading, -60)
+                                
+                                VStack(alignment: .leading) {
+                                    if let patientData = patientData{
+                                        Text("\(patientData.name)")
+                                        Text("\(patientData.phone)")
+                                    }
+                                }
+                                .padding(.leading)
                             }
-                            .padding(.leading)
+                            Spacer()
                             
-                            
-                        }
-                        Spacer()
-                        
-                        Section {
-                            List(patients) { patient in
-                                NavigationLink(destination: MedicalDataView(APatient: patient)) {
+                            Section {
+                                List {
+                                    NavigationLink(destination: MedicalDataView()) {
                                     Image(systemName: "aqi.medium")
                                         .foregroundColor(Color(red: 148/255, green: 28/255, blue: 47/255))
                                     Text("Detalles personales médicos")
                                 }
-                                NavigationLink(destination: HealthDataDetails()) {
-                                    Image(systemName: "heart.fill")
-                                        .foregroundColor(Color(red: 148/255, green: 28/255, blue: 47/255))
-                                    Text("Detalles de datos de salud")
+                                    NavigationLink(destination: HealthDataDetails()) {
+                                        Image(systemName: "heart.fill")
+                                            .foregroundColor(Color(red: 148/255, green: 28/255, blue: 47/255))
+                                        Text("Detalles de datos de salud")
+                                    }
+                                    
                                 }
-
+                                
                             }
-                            
-                        }
-                        Section {
-                            List {
-                                NavigationLink(destination: DoctorDetailsView()) {
-                                    Image(systemName: "book.pages")
-                                        .foregroundColor(Color(red: 26/255, green: 26/255, blue: 102/255))
-                                    Text("Detalles de médico")
-                                }
-                                NavigationLink(destination: DoctorDetailsView()) {
-                                    Image(systemName: "person.crop.circle.badge.plus")
-                                        .foregroundColor(Color(red: 26/255, green: 26/255, blue: 102/255))
-                                    Text("Enlazar a un médico")
+                            Section {
+                                List {
+                                    NavigationLink(destination: DoctorDetailsView()) {
+                                        Image(systemName: "book.pages")
+                                            .foregroundColor(Color(red: 26/255, green: 26/255, blue: 102/255))
+                                        Text("Detalles de médico")
+                                    }
+                                    NavigationLink(destination: DoctorDetailsView()) {
+                                        Image(systemName: "person.crop.circle.badge.plus")
+                                            .foregroundColor(Color(red: 26/255, green: 26/255, blue: 102/255))
+                                        Text("Enlazar a un médico")
+                                    }
+                                    
                                 }
                                 
                             }
                             
+                            //                        Section {
+                            //                            List {
+                            //
+                            //
+                            //                            }
+                            //                        }
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
                         }
+                        .padding()
                         
-//                        Section {
-//                            List {
-//                                
-//                                
-//                            }
-//                        }
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()        
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
+                        // 124,152,159
+                        //.navigationTitle("Mi perfil")
+                        //.foregroundColor(Color(red: 124/255, green: 152/255, blue: 159/255))
+                        
                     }
-                    .padding()
                     
-                    // 124,152,159
-                    //.navigationTitle("Mi perfil")
-                    //.foregroundColor(Color(red: 124/255, green: 152/255, blue: 159/255))
-                    
+                    .background(Color.clear)
                 }
                 
+                .onAppear {
+                    handlePatientData()
+                }
                 .background(Color.clear)
+                //.navigationTitle("Mi perfil")
+                //            .task {
+                //                await getMedicalData()
             }
-            
-            .onAppear {
-                handlePatientData()
-            }
-            .background(Color.clear)
-            //.navigationTitle("Mi perfil")
-//            .task {
-//                await getMedicalData()
-//            }
             
         }
         
     }
+}
 
 
 struct ProfileView_Previews: PreviewProvider {
