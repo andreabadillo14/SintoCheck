3823448567a8117bfef51450ab1fd5d0e4e79d58
sebastian <sebramirezcordero@gmail.com>
2023-11-22 14:08:10 -0600

    .

--- app-sintocheck/SintoCheck/SintoCheck/funcionesUtilidad/requests.swift
+++ app-sintocheck/SintoCheck/SintoCheck/funcionesUtilidad/requests.swift
@@ -98,7 +98,7 @@ func addNoteAPI(title: String, content: String, completion: @escaping (Note?) ->
 //para view DoctorDetailsView
 func fetchDoctor(completion: @escaping ([Doctor]?) -> Void) {
     //obtener id del usuario que inicio sesion esta hardcodeado ahora
-    guard let url = URL(string: "https://sintocheck-backend.vercel.app/doctorPatientRelationship/654ea09fd9fb791b4b7f087c") else {return}
+    guard let url = URL(string: "https://sintocheck-backend.vercel.app/doctorPatientRelationship/655d2e65fe22ef65b83c94e0") else {return}
     var request = URLRequest(url: url)
     //obtener token del inicio de sesion esta hardcodeado ahora.
     request.addValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NWQyZTY1ZmUyMmVmNjViODNjOTRlMCIsIm5hbWUiOiJwYWNpZW50ZSBQcnVlYmEiLCJwaG9uZSI6IjEyMzQ0MzIxIiwiaWF0IjoxNzAwNjA1NTUwLCJleHAiOjE3MDE4MTUxNTB9.54PgoD0Vd8xLPWltOAXSDW7iCJGFaRUo-TQANmrPw9k", forHTTPHeaderField: "Authorization")
